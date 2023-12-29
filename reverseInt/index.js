bde5c3f9e71eaecc7b5261b9ced7341976f2b82d
Arthur Fernandez <arthur@snooget.com>
2023-12-29 15:17:17 -0800

    Reverse int exercise done

--- reverseInt/index.js
+++ reverseInt/index.js
@@ -6,9 +6,64 @@
 //   reverseInt(100) === 1
 //   reverseInt(-13) === -31
 //   reverseInt(-100) === -1
+/*My attempt which draws upon inspo from the reverse int 
 
-function reverseInt(n) {}
 
+function reverseInt(n) {
+  return n.toString().split("").reverse().join("");
+  let num = "";
+  for (let i = 0; i <= n.length; i--) {
+    const char = n[i];
+    num = char + num;
+  }
+  return num;
+}
+*/
+/*
+ChatGpt Help after mine:
+*/
+/*
+function reverseInt(n) {
+  const revString = n.toString().split("").reverse().join("");
+  const revNumber = parseInt(revString);
+
+  return n < 0 ? -revNumber : revNumber;
+}
+*/
+/* Below is coding Kevin's answer  */
+function reverseInt(n) {
+  let reversed = n.toString().split("").reverse().join("");
+  reversed = parseInt(reversed);
+
+  if (n < 0) {
+    return reversed * -1;
+  }
+  return reversed;
+}
+
+/* Below is a correct attempt made by a user to not use methods to solve the reverse int
+function reverseInt(n) {
+  let number = n;
+  let reversed = 0;
+
+  if (number < 0) {
+    number = -number;
+    while (number > 0) {
+      let lastDigit = number % 10;
+      reversed = reversed * 10 + lastDigit;
+      number = Math.floor(number / 10);
+    }
+    return -reversed;
+  } else {
+    while (number > 0) {
+      let lastDigit = number % 10;
+      reversed = reversed * 10 + lastDigit;
+      number = Math.floor(number / 10);
+    }
+    return reversed;
+  }
+}
+*/
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
 //    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
